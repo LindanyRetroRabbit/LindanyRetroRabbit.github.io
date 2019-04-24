54dbc1d777a34a300c2874f666392334eca55848
Lindani Mabaso <lmabaso@retrorabbit.co.za>
2019-04-24 20:23:04 +0200

    Changes to the insert

--- server.php
+++ server.php
@@ -1,23 +1,79 @@
 <?php
 	
-	  session_start();
-	  $name = "";
-	  $address = "";
-	  $id = 0;
+session_start();
+	  // $name = "";
+	  // $address = "";
+	  // $id = 0;
+	  $id=0;
+	  $dname= "";
+	  $dsurname="";
+	  $dgender="";
+	  $dcontact="";
+	  $demail="";
+	  $church="";
+	  $topics="";
+	  $sname="";
+	  $ssurname="";
+	  $sdob="";
+	  $sgender="";
+	  $saddress="";
+	  $scontact="";
+	  $sstatus="";
+	  $start_date="";
+	  $end_date="";
+	  $dcomment="";
 	//connect database
 $db = mysqli_connect('localhost','root','','crud');
 
 //if save button clicked
    if(isset($_POST['save'])){
-	  $name = $_POST['name'];
-	  $address = $_POST['address'];
 	  
-	  $query = "INSERT INTO info(name,address) VALUES ('$name','$address')";
+	  $query = "INSERT INTO gauteng(
+	     person_id,
+	     discipler_name,
+	  	discipler_surname,
+	  	discipler_gender,
+	  	discipler_contact,
+	  	discipler_email,
+	  	discipler_church,
+	  	discipler_topics,
+	  	disciple_name,
+	  	disciple_surname,
+	  	disciple_dob,
+	  	disciple_gender,
+	  	disciple_address,
+	  	disciple_contact,
+	  	disciple_status,
+	  	start_date,
+	  	end_date,
+	  	disciple_comment) 
+
+	  	VALUES 
+
+	  ('$id',
+	  '$dname',
+	  '$dsurname',
+	  '$dgender',
+	  '$dcontact',
+	  '$demail',
+	  '$church',
+	  '$topics',
+	  '$sname',
+	  '$ssurname',
+	  '$sdob',
+	  '$sgender',
+	  '$saddress',
+	  '$scontact',
+	  '$sstatus',
+	  '$start_date',
+	  '$end_date',
+	  '$dcomment')";
+
 	  mysqli_query($db,$query);
-	  $_SESSION['msg'] = "Address Saved";
+	  $_SESSION['msg'] = "Discipleship information succesfully captured";
 	  header('location: index.php'); // redirecting to index after inserting
 }
 	//retrieve records
-	$results = mysqli_query($db,"SELECT * FROM info");
-   
+	$results = mysqli_query($db,"SELECT * FROM gauteng");
+   	
 ?>
